7e4b5ae8d523c87235005529bee630d6a33c3b2b
Talhajjaj <>
2021-04-14 19:43:39 +0000

    edit

---
+++ javascript/app.js
@@ -0,0 +1,28 @@
+alert('welcome to our website');
+console.log('welcom to our website');
+
+var feelings = prompt('please enter how is your feelings (happy,sad)?') ;
+//console.log ('feelings');
+// alert ('feelings')
+if(feelings== 'happy')
+{console.log('cool')}
+else  
+{console.log ('okay')}
+
+var purpose = prompt('please enter your purpose of visiting us?');
+console.log('purpose');
+
+
+// var foodType = prompt('please enter a food type (mansaf , shawarmah)?')
+
+// if(foodType =='mansaf')
+// {
+//   var image = '<img src="https://www.experiencejordan.com/wp-content/uploads/2016/10/Blog-Headers2-1.jpg" alt= "mansaf"/>';
+//   //create mansaf image
+//   document.write(image);
+  
+// }else if(foodType =='shawarmah'){
+//   //create shawarmah image
+//   var image2 = '<img src="img/shawarmah.jpg" alt= "shawarmah"/>';
+//   document.write(image2);
+// }
