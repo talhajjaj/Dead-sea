5eb3125ecaf785bf9d5cf1e3e248bcd814b4e7d1
Talhajjaj <>
2021-04-18 11:56:26 +0000

    last edit enshallah

--- javascript/app.js
+++ javascript/app.js
@@ -1,70 +1,70 @@
-// alert('welcome to our website');
-// console.log('welcom to our website');
+alert('welcome to our website');
+console.log('welcom to our website');
 
-// var feelings = prompt('please enter how is your feelings (happy,sad)?') ;
-// //console.log ('feelings');
-// // alert ('feelings')
-// if(feelings== 'happy')
-// {console.log('cool')}
-// else  
-// {console.log ('okay')}
+var feelings = prompt('please enter how is your feelings (happy,sad)?') ;
+//console.log ('feelings');
+// alert ('feelings')
+if(feelings== 'happy')
+{console.log('cool')}
+else  
+{console.log ('okay')}
 
-// var purpose = prompt('please enter your purpose of visiting us?');
-// console.log('purpose');
+var purpose = prompt('please enter your purpose of visiting us?');
+console.log('purpose');
 
 
-// // var visit = prompt('please enter what you will visit (resturant , hotels)?')
+// var visit = prompt('please enter what you will visit (resturant , hotels)?')
 
-// // if(visit =='resturant')
-// // {
-// //   var image = '<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaLQFNYQVWAo5Mmuv4HSXAgx4aKiIyJlt-9Q&usqp=CAU" alt= "resturant"/>';
-// //   //create resturant image
-// //   document.write(image);
-  
-// // }
-// // else if(visit =='hotels'){
-// //   //create hotels image
-// //   var image2 = '<img src="https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_1300,q_auto,w_2000/itemimages/25/88/25887_v10.jpeg" alt= "hotels"/>';
-// //   document.write(image2);
-// // }
-// // //ask the user what he will visit( resturant, hotels).
-// // // if the user enter resturant print a resturant image
-
-// // //if the user enter hotels print a hotels image
-// var visit = prompt('please enter what you will visit (resturant , hotels)?');
-
-// while (visit !== 'resturant' &&  visit !== 'hotels') {
-//   visit = prompt('please enter how many times you will visit (resturant , hotels?');
-// }
-// console.log(visit);
-
-// // function visit(asking) 
-// var asking = function (visit)
+// if(visit =='resturant')
 // {
-//   var asking = prompt('please enter what you will' + visit+ '?');
+//   var image = '<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaLQFNYQVWAo5Mmuv4HSXAgx4aKiIyJlt-9Q&usqp=CAU" alt= "resturant"/>';
+//   //create resturant image
+//   document.write(image);
   
-//   return asking;
+// }
+// else if(visit =='hotels'){
+//   //create hotels image
+//   var image2 = '<img src="https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_1300,q_auto,w_2000/itemimages/25/88/25887_v10.jpeg" alt= "hotels"/>';
+//   document.write(image2);
+// }
+// //ask the user what he will visit( resturant, hotels).
+// // if the user enter resturant print a resturant image
+
+// //if the user enter hotels print a hotels image
+var visit = prompt('please enter what you will visit (resturant , hotels)?');
+
+while (visit !== 'resturant' &&  visit !== 'hotels') {
+  visit = prompt('please enter how many times you will visit (resturant , hotels?');
+}
+console.log(visit);
+
+// function visit(asking) 
+var asking = function (visit)
+{
+  var asking = prompt('please enter what you will' + visit+ '?');
+  
+  return asking;
+  }
+
+// // console.log('visit ' 
+// // + visit);
+
+// var visitNumber = prompt('please enter how many times you will visit (resturant , hotels?');
+
+
+
+//   if (visit == 'resturant') {
+//     for (var i = 1; i <= visitNumber; i++) {
+
+//     var image = '<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaLQFNYQVWAo5Mmuv4HSXAgx4aKiIyJlt-9Q&usqp=CAU" alt=" alt= "resturant"/>';
+//     //create resturant image
+//     document.write(image);
+// }
+//   } else if (visit == 'hotels') {
+//     for (var i = 0; i <= visitNumber; i++) {
+//     //create hotels image
+//     var image2 = '<img src="https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_1300,q_auto,w_2000/itemimages/25/88/25887_v10.jpeg" alt= "hotels"/>';
+
+//     document.write(image2);
+//     }
 //   }
-
-// // // console.log('visit ' 
-// // // + visit);
-
-// // var visitNumber = prompt('please enter how many times you will visit (resturant , hotels?');
-
-
-
-// //   if (visit == 'resturant') {
-// //     for (var i = 1; i <= visitNumber; i++) {
-
-// //     var image = '<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaLQFNYQVWAo5Mmuv4HSXAgx4aKiIyJlt-9Q&usqp=CAU" alt=" alt= "resturant"/>';
-// //     //create resturant image
-// //     document.write(image);
-// // }
-// //   } else if (visit == 'hotels') {
-// //     for (var i = 0; i <= visitNumber; i++) {
-// //     //create hotels image
-// //     var image2 = '<img src="https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_1300,q_auto,w_2000/itemimages/25/88/25887_v10.jpeg" alt= "hotels"/>';
-
-// //     document.write(image2);
-// //     }
-// //   }
